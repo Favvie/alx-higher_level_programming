326c666c2b38a67031cf5c731e2bbd9666c00dc0
Favvie <favourprecious3@gmail.com>
2022-07-05 04:06:28 +0100

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """if an object is a subclass of a class"""
-    if (issubclass(type(obj), a_class)):
+    if (issubclass(type(obj), a_class) and type(obj) is not a_class):
         return True
     else:
         return False
