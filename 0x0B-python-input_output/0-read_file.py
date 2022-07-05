c3adc43859c4cdfd07466f2323ce0c7718372bb4
Favvie <favourprecious3@gmail.com>
2022-07-05 15:30:25 +0100

    add files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This is a module for read file function"""
 def read_file(filename=''):
+    """a function that read files"""
     with open(filename, encoding="utf-8") as f:
         f.read()
