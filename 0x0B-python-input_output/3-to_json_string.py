af47cb10a46b98304f49073eedd116bf277ccaab
Favvie <favourprecious3@gmail.com>
2022-07-06 04:35:06 +0100

    task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,7 +2,8 @@
 """ a module that converts to json"""
 
 
+import json
 def to_json_string(my_obj):
     """a function that converts to json"""
-    obj = json.dumps(x)
+    obj = json.dumps(my_obj)
     return obj
