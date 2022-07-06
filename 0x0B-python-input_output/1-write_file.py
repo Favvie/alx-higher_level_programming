61999ccbc0770984c8e23aaacad295e53a89dfe9
Favvie <favourprecious3@gmail.com>
2022-07-06 04:16:50 +0100

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ a module that writes file """
+
+
 def write_file(filename="", text=""):
+    """ a function that writes to a file"""
     with open(filename, mode='w', encoding="utf-8") as f:
         nb = f.write(text)
         return nb
