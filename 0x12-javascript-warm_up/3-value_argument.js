4b673f4c273770744db4f59c7e78e23bddf67bad
Favvie <favourprecious3@gmail.com>
2022-09-06 09:10:29 +0100

    update process

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
+const process = require('process');
 
-if (argv[2]) {
-  console.log(argv[2]);
+if (process.argv[2]) {
+  console.log(process.argv[2]);
 } else {
   console.log('No argument');
 }
