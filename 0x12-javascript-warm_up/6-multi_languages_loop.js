d56c21c82fc26be77cbc97ed2e8169d26be814bf
Favvie <favourprecious3@gmail.com>
2022-09-06 09:51:58 +0100

    use a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+let array_of_string = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for(i = 0; i < array_of_string.length; i++) {
+  console.log(array_of_string[i]);
+}
