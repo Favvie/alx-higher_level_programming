0faf93ee3fb8f57e53d3e3497bf0e28c575d64db
Favvie <favourprecious3@gmail.com>
2022-09-13 14:03:29 +0100

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-"""
-This module runs a script to fetch all states table
-in the database provided
-"""
-import MySQLdb
-from sys import argv
+"""the module of life"""
 
+import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    states = cur.fetchall()
-    for state in states:
-        print(state)
-    cur.close()
-    db.close()
+    db = MySQLdb.connect(host='localhost',
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM states')
+
+    for x in cursor:
+        print(x)
