9d277d7477a6ce57083eae714b3db829995310a4
Favvie <favourprecious3@gmail.com>
2022-09-13 14:24:07 +0100

    filter states using like keyword

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
-"""
-This module runs a script to fetch all states table
-in the database provided where name starts with 'N'
-"""
+""" a module for life """
 import MySQLdb
-from sys import argv
+import sys
 
-
-if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%'")
-    states = cur.fetchall()
-    for state in states:
-        if state[1][0] == 'N':
-            print(state)
-    cur.close()
-    db.close()
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM states WHERE name LIKE "N%"')
+    for x in cursor:
+        print(x)
