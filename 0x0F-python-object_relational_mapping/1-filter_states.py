f4a84ac8c1730f754622554cb02e9c7d03eb9e7d
Favvie <favourprecious3@gmail.com>
2022-09-13 14:33:02 +0100

    test case for small letter n

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost',
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute('SELECT * FROM states WHERE name LIKE "N%"')
+    cursor.execute('SELECT * FROM states WHERE name LIKE BINARY "N%"')
     for x in cursor:
         print(x)
