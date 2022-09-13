9c3dfd50d03901b0337813128a7a1b984e1fefb2
Favvie <favourprecious3@gmail.com>
2022-09-13 16:27:21 +0100

    filter state

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
-"""
-This module runs a script to fetch all states table
-in the database provided where name matches a provided value
-This script is succeptible to SQL injection attack
-"""
+""" a module"""
 import MySQLdb
-from sys import argv
+import sys
 
-
-if __name__ == "__main__":
-    if len(argv) == 5:
-        db = MySQLdb.connect(host='localhost', user=argv[1], passwd=argv[2],
-                             db=argv[3], charset='utf8')
-        cur = db.cursor()
-        cur.execute("SELECT * FROM states WHERE name = '{:s}' ORDER BY id"
-                    .format(argv[4]))
-        states = cur.fetchall()
-        for state in states:
-            if state[1] == argv[4]:
-                print(state)
-        cur.close()
-        db.close()
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost',
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM states WHERE name="{:s}" ORDER BY id'
+                   .format(sys.argv[4]))
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
