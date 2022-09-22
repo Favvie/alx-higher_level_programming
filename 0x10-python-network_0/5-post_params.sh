09cc43fae5c184f4c39bcc2fb144e78398e7b1f2
Favvie <favourprecious3@gmail.com>
2022-09-22 21:50:19 +0100

    shellcheck update

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # post data using curl
-curl -sd "email=test@gmail.com&subject=I%20will%20always%20be%20here%20\
-for%20PLD"  "$1"
+curl -sd "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD"  "$1"
