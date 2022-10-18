574d1bd0e27ef9095e42ecd1b14feaf312c64432
Favvie <favourprecious3@gmail.com>
2022-10-18 22:24:59 +0100

    task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,9 +2,9 @@
 
 const request = require('request');
 
-request({ url: 'https://swapi-api.hbtn.io/api/films/{process.argv[2]}', json: true }, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    console.loge(body.title);
+request({ url: `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, json: true }, function (error, response, body) {
+  if (!error) {
+    return console.log(body.title);
   } else {
     console.error(error);
   }
