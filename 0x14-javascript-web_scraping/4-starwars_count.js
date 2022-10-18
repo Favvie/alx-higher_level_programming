be571cad351ab244ba3e44fb5d12a5a826e1acf7
Favvie <favourprecious3@gmail.com>
2022-10-18 22:18:18 +0100

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,17 +2,22 @@
 
 const request = require('request');
 
+let count = 0;
 request({ url: process.argv[2], json: true }, function (error, response, body) {
   if (!error && response.statusCode === 200) {
     const dataArray = body.results;
-    let count = 0;
     for (const film of dataArray) {
-      if (film.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-        count = count + 1;
+      //      if (film.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+      const item = film.characters;
+      for (let j = 0; j < item.length; j++) {
+        const val = item[j].split('/');
+        if (val[val.length - 2] === '18') {
+          count++;
+          break;
+        }
       }
+      //        count = count + 1;
     }
-    console.log(count);
-  } else {
-    console.error(error);
   }
+  console.log(count);
 });
